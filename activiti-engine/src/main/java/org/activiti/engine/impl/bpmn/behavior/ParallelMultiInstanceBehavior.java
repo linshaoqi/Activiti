035264d6deec9a427e4c4fbb03d7c2f482bbc1a5
dhrn <dharan.g@muraai.com>
2019-11-12 12:43:42 +0530

    [Multi instance] The audit log 'ACTIVITY_COMPLETED' for Service-task is not logged

--- activiti-engine/src/main/java/org/activiti/engine/impl/bpmn/behavior/ParallelMultiInstanceBehavior.java
+++ activiti-engine/src/main/java/org/activiti/engine/impl/bpmn/behavior/ParallelMultiInstanceBehavior.java
@@ -212,8 +212,10 @@ public class ParallelMultiInstanceBehavior extends MultiInstanceActivityBehavior
         executionToUse.setMultiInstanceRoot(false);
         Context.getAgenda().planTakeOutgoingSequenceFlowsOperation(executionToUse, true);
       }
+      postMultiInstanceIterationCompleteEvent(executionEntity);
 
     } else {
+      postMultiInstanceIterationCompleteEvent(executionEntity);
       removeLocalLoopVariable(execution, getCollectionElementIndexVariable());
       execution.setMultiInstanceRoot(false);
       super.leave(execution);
